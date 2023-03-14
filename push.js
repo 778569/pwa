84c3c89488f9c4ba153c27dcd54706b2976f8912
kavinda <kavinda@inexisconsulting.com>
2023-03-14 13:59:25 +0530

    fix

--- push.js
+++ push.js
@@ -18,11 +18,10 @@ let vapidKeys = {
   let sub =
   
   {
-    endpoint:
-  "https://fcm.googleapis.com/fcm/send/eVOYZtW14KY:APA91bFeyTn7rThHgkiPhtJ-2taLO3vzctQCKSrXMsnHSCCxqXERwFi-Jl9I5U-Je2RUJ1TbfhB14fo3AiOYBOaJvNQ6LmCLbqUrgtzaEZRdf0cFRiQXHB8JrERxIpHoDvhW9uwvPl7I",
-  expirationTime:null,
-  keys:{
-    p256dh:"BFN1JoJUp1teI3bsWRMunwWE5TpdxP3HEGK5ZURNye_1UuNNC-tYcSz47Y0ZGw3sJMJ2fzJQ5CTH_dNzKXXpsf4",
+    endpoint:"https://fcm.googleapis.com/fcm/send/eVOYZtW14KY:APA91bFeyTn7rThHgkiPhtJ-2taLO3vzctQCKSrXMsnHSCCxqXERwFi-Jl9I5U-Je2RUJ1TbfhB14fo3AiOYBOaJvNQ6LmCLbqUrgtzaEZRdf0cFRiQXHB8JrERxIpHoDvhW9uwvPl7I",
+    expirationTime:null,
+    keys:{
+      p256dh:"BFN1JoJUp1teI3bsWRMunwWE5TpdxP3HEGK5ZURNye_1UuNNC-tYcSz47Y0ZGw3sJMJ2fzJQ5CTH_dNzKXXpsf4",
   auth:"pGJURRft5Cq7ajccrYAFsA"}}
 
 
