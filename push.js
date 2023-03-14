fd2707ce121428bdbe99d142bc878cf33c2e8ee6
kavinda <kavinda@inexisconsulting.com>
2023-03-14 12:09:49 +0530

    fix

--- push.js
+++ push.js
@@ -5,20 +5,19 @@ let vapidKeys = {
     privateKey: 'ooQ86HW2BLqII0Ag7V5nf2v3C1eXMKlneXXYCmuKWr0'
   }
 
-  // push.setVapidDetails(
-  //   'kavindabandara94@gmail.com',
-  //   vapidKeys.publicKey,
-  //   vapidKeys.privateKey
-  // );
+  push.setVapidDetails(
+    'kavindabandara94@gmail.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+  );
 
   let sub =
-  {
-  endpoint:"https://fcm.googleapis.com/fcm/send/eVOYZtW14KY:APA91bFeyTn7rThHgkiPhtJ-2taLO3vzctQCKSrXMsnHSCCxqXERwFi-Jl9I5U-Je2RUJ1TbfhB14fo3AiOYBOaJvNQ6LmCLbqUrgtzaEZRdf0cFRiQXHB8JrERxIpHoDvhW9uwvPl7I",
-  expirationTime :null,
-  keys:{
-    p256dh:"BFN1JoJUp1teI3bsWRMunwWE5TpdxP3HEGK5ZURNye_1UuNNC-tYcSz47Y0ZGw3sJMJ2fzJQ5CTH_dNzKXXpsf4",
-    auth:"pGJURRft5Cq7ajccrYAFsA"
-  }
-}
+  
+    {
+      endpoint:"https://wns2-sg2p.notify.windows.com/w/?token=BQYAAAARnSLCI240GFkdxUDpLyTLCjPnOqnQYHaFzjAh53Ir0GO8iUUdJaq4UmwQrYNGqcKD5HuSxbbLqOerWwfVbRlkp9x8pKqE0UbDI8AhMMYVMO6SE1CwdrfTlFHy3d5R%2f8OTmLIQyYSwb4TukW8JoaUIdDM1VRV1rSoL8AHjeyO%2fRSqsBykdOGXVjmfcgRSuOUh1bwvdvfGk%2fs03qu86l1qCjz8vEYlCWW37cf3jf1hk3mF2y1I4df9InHv0kV0y1deQ6%2bWq7KHBcugR2h2nfkM6%2bJrRavhzvIwf8RRV6HrJ4N28tFARCNqIcv82Fs29IWngZq4Bh8%2b9yiULtY%2b6aFGpzVwutot53%2bcSkXkDUUzpbA%3d%3d",
+      expirationTime:null,
+      keys:{
+        p256dh:"BHqyHLs4MzhIQRaRonlol5YHfDZy8pDMysBrraE7t5L63g4MgOgul-3e-Bw-SiHXV0kYCeuzWJJbR6oR8fTyZ0Y","auth":"6KFYpnQFkMgFkSAKyD8QHQ"}}
+
 
 push.sendNotification(sub,'test message')
