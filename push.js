c75fec289b9073e7da6df87670a83d662644b80e
kavinda <kavinda@inexisconsulting.com>
2023-03-14 10:05:51 +0530

    fix

--- push.js
+++ push.js
@@ -5,11 +5,20 @@ let vapidKeys = {
     privateKey: 'ooQ86HW2BLqII0Ag7V5nf2v3C1eXMKlneXXYCmuKWr0'
   }
 
-  webpush.setVapidDetails(
-    'kavindabandara94@gmail.com',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
-  );
+  // push.setVapidDetails(
+  //   'kavindabandara94@gmail.com',
+  //   vapidKeys.publicKey,
+  //   vapidKeys.privateKey
+  // );
 
-  let sub = {};
-  push.sendNotification(sub, 'Your Push Payload Text');
+  let sub =
+  {
+  endpoint:"https://fcm.googleapis.com/fcm/send/eVOYZtW14KY:APA91bFeyTn7rThHgkiPhtJ-2taLO3vzctQCKSrXMsnHSCCxqXERwFi-Jl9I5U-Je2RUJ1TbfhB14fo3AiOYBOaJvNQ6LmCLbqUrgtzaEZRdf0cFRiQXHB8JrERxIpHoDvhW9uwvPl7I",
+  expirationTime :null,
+  keys:{
+    p256dh:"BFN1JoJUp1teI3bsWRMunwWE5TpdxP3HEGK5ZURNye_1UuNNC-tYcSz47Y0ZGw3sJMJ2fzJQ5CTH_dNzKXXpsf4",
+    auth:"pGJURRft5Cq7ajccrYAFsA"
+  }
+}
+
+push.sendNotification(sub,'test message')
--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!',{});
+// self.addEventListener('push', () => {
+//   self.registration.showNotification('Hello world!',{});
+// });
+
+self.addEventListener('push', function(event) {
+  var options= {
+
+    body: 'This is notification was generated from a push',
+    icon: 'images/example.png',
+    vibrate:[100,50,100],
+    date: {
+      dateOfArrival:Date.now(),
+      primaryKey: '2'
+      },
+  
+  actions: [
+    {action:'explore',title:'Explore this new world',icon :'images/checkmark.png'},
+    {action:'close',title:'close',icon :'images/xmark.png'},
+  ]
+};
+
+event.waitUntil(
+  self.registration.showNotification('Hello world!',options)
+);
 });
